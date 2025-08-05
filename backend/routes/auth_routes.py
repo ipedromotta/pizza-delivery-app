0f89fab95b459d61a9d166a3f3b8ed4762bba396
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-08-04 21:58:28 -0300

    ✨ feat: implementacao da rota de registro

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,9 +1,37 @@
-from fastapi import APIRouter
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException
+from db import Session
+from models import User
+from core import Criptografia
+from schemas import UserSchema
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.get("/")
-async def auth():
-    return {"message": "teste"}
+async def home():
+    return {"message": "teste", "is_authenticated": False}
+
+@auth_router.post("/register")
+def register_user(usuario_schema: UserSchema):
+    with Session.get_session() as session:
+        usuario = session.query(User).filter(User.email == usuario_schema.email).first()
+        
+        if usuario:
+            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Email de usuário já cadastrado")
+        
+        else:
+            senha_criptografada = Criptografia.criptografar(usuario_schema.senha)
+            novo_usuario = User(
+                nome=usuario_schema.nome,
+                email=usuario_schema.email,
+                senha=senha_criptografada,
+                ativo=usuario_schema.ativo,
+                admin=usuario_schema.admin
+                )
+            session.add(novo_usuario)
+            session.commit()
+            return {"message": f"Usuário criado com sucesso! {usuario_schema.email}"}
+        
+    
