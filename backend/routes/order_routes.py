68e5c4873bb0e8330b1e2f3f251d95d99c9855b5
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-08-15 21:23:31 -0300

    ✨ feat: add controle de acesso e rota cancelar

--- backend/routes/order_routes.py
+++ backend/routes/order_routes.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
-from schemas import OrderSchema
-from models import Order
+from http import HTTPStatus
+from fastapi import APIRouter, Depends, HTTPException
+
 from db import Session
+from core import check_token
+from models import Order, User
+from schemas import OrderSchema
 
 
-order_router = APIRouter(prefix="/orders", tags=["orders"])
+order_router = APIRouter(prefix="/orders", tags=["orders"], dependencies=[Depends(check_token)])
 
 
 @order_router.get("/")
@@ -21,3 +24,21 @@ async def create_order(pedido_schema: OrderSchema):
         
         return {"message": f"Pedido criado com sucesso. ID do pedido: {novo_pedido.id}"}
     
+@order_router.post("/cancel/{id_pedido}")
+async def cancel_order(id_pedido: int, usuario: User = Depends(check_token)):
+    with Session.get_session() as session:
+        pedido = session.query(Order).filter(Order.id == id_pedido).first()
+        
+        if not pedido:
+            raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Pedido não encontrado")
+        
+        if not usuario.admin or usuario.id != pedido.usuario:
+            raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Você não tem autorização para cancelar esse pedido.")
+        
+        pedido.status = "CANCELADO"
+        session.commit()
+        
+        return {
+            "message": f"Pedido número {pedido.id} cancelado com sucesso",
+            "order": pedido
+        }
