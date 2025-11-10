68127e7a3f08826971d7cbeee79e9cd05533e041
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-11-10 20:35:27 -0300

    ✨ feat: add paginas Login/Cadastro/Esqueceu senha

---
+++ frontend/src/views/Cadastro.vue
@@ -0,0 +1,116 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const errors = ref('')
+const form = ref({
+  first_name: '',
+  email: '',
+  username: '',
+  password: '',
+  password_confirm: '',
+})
+
+async function onSubmit() {
+  errors.value = ''
+
+  if (form.value.password !== form.value.password_confirm) {
+    errors.value = 'As senhas não são iguais. Tente novamente'
+  }
+
+  if (!errors.value.length) {
+    await axios.post('/api/v1/users/', form.value)
+    .then((res) => {
+      router.push('/login')
+    })
+    .catch((error) => {
+      if ( error.response ) {
+        for (const property in error.response.data) {
+          errors.value += `${error.response.data[property]}\n`
+        }
+
+        console.log(JSON.stringify(error.response.data))
+      } else if ( error.message ) {
+        errors.value = 'Algo deu errado. Por favor tente novamente.'
+
+        console.log(JSON.stringify(error))
+      }
+    })
+  }
+}
+
+</script>
+
+<template>
+  <div class="text-center body">
+    <main class="form-signup">
+      <form class="space-between" @submit.prevent="onSubmit">
+        <img class="mb-4" src="@/assets/logo.svg" alt="" width="150">
+        <h1 class="h3 mb-3 fw-normal">Cadastre-se</h1>
+
+        <div class="form-floating mb-2">
+          <input v-model="form.first_name" required type="text" maxlength="15" class="form-control" id="floatingName" placeholder="Seu nome">
+          <label for="floatingName">Nome</label>
+        </div>
+
+        <div class="form-floating mb-2">
+          <input v-model="form.email" required type="email" maxlength="50" class="form-control" id="floatingEmail" placeholder="email@exemplo.com">
+          <label for="floatingEmail">Email</label>
+        </div>
+
+        <div class="form-floating mb-2">
+          <input v-model="form.username" required type="text" maxlength="15" class="form-control" id="floatingInput" placeholder="seu-usuario">
+          <label for="floatingInput">Usuário</label>
+        </div>
+
+        <div class="form-floating mb-2">
+          <input v-model="form.password" required type="password" class="form-control" id="floatingPassword" placeholder="senha">
+          <label for="floatingPassword">Senha</label>
+        </div>
+
+        <div class="form-floating">
+          <input v-model="form.password_confirm" required type="password" class="form-control" id="floatingPassword2" placeholder="confirme sua senha">
+          <label for="floatingPassword2">Confirme sua senha</label>
+        </div>
+
+        <div class="alert alert-danger" role="alert" v-if="errors.length">
+          <span v-for="error, index in errors" :key="index">{{ error }}</span>
+        </div>
+
+        <button :class="`w-100 btn btn-lg btn-danger ${errors.length?'mt-0': 'mt-5'}`" type="submit">Cadastrar</button>
+        
+        <div>
+          <p class="mt-5">
+            Ou <RouterLink to="/login" class="text-danger fw-bold link">clique aqui</RouterLink> para logar
+          </p>
+        </div>
+      </form>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  display: flex;
+  align-items: center;
+  margin-top: 10vh;
+}
+
+.form-signup {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.link {
+  text-decoration: none;
+}
+.link:hover{
+  cursor: pointer;
+  color: #aaaaaa!important;
+  transition: ease-in 0.2s;
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,91 @@
+<script setup>
+// import axios from 'axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+// import { usePageStore } from '../stores/page';
+
+// const pageStore = usePageStore()
+const router = useRouter()
+const errors = ref('')
+const form = ref({
+  username: '',
+  password: ''
+})
+
+function onSubmit() {}
+
+</script>
+
+<template>
+  <div class="text-center body">
+    <main class="form-signin">
+      <form @submit.prevent="onSubmit">
+        <img class="mb-4" src="@/assets/logo.svg" alt="" width="150">
+        <h1 class="h3 mb-3 fw-normal">Fazer login</h1>
+
+        <div class="form-floating">
+          <input v-model="form.username" type="text" required maxlength="15" class="form-control" id="floatingInput" placeholder="seu-usuario">
+          <label for="floatingInput">Usuário</label>
+        </div>
+
+        <div class="form-floating">
+          <input v-model="form.password" type="password" required class="form-control" id="floatingPassword" placeholder="senha">
+          <label for="floatingPassword">Senha</label>
+        </div>
+
+        <p :class="`small pb-lg-2 ${errors.length? 'mb-0' : 'mb-5'}`"><RouterLink class="text-danger link" to="/esqueceu-senha">Esqueceu sua senha?</RouterLink></p>
+
+        <div class="alert alert-danger" role="alert" v-if="errors.length">
+          <span v-for="error, index in errors" :key="index">{{ error }}</span>
+        </div>
+        
+        <button class="w-100 btn btn-lg btn-danger" type="submit">Entrar</button>
+        
+        <div>
+          <p class="mt-5">Não tem uma conta? <RouterLink to="/cadastrar" class="text-danger fw-bold link">Cadastre-se</RouterLink>
+          </p>
+        </div>
+      </form>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  display: flex;
+  align-items: center;
+  margin-top: 3vh;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="text"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+.link {
+  text-decoration: none;
+}
+.link:hover{
+  cursor: pointer;
+  color: #aaaaaa!important;
+  transition: ease-in 0.2s;
+}
+</style>
