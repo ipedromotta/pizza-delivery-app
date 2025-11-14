33619612f33c830f5fc636321cd1a5ac24a2705f
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-11-13 23:27:06 -0300

    ✨ feat: criacao pizza item

---
+++ frontend/src/components/PizzaItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps({
+  nome: String,
+  preco: Number,
+  imagem: String
+}) 
+</script>
+
+<template>
+    <div class="col">
+        <div class="card p-3 text-center mb-3">
+            <img :src="imagem" class="img-fluid mb-2" />
+            <h5>{{ nome }}</h5>
+            <p>{{ preco }} R$</p>
+            <button class="btn btn-danger">
+                <i class="bi bi-bag-plus-fill"></i>
+                Comprar
+            </button>
+        </div>
+    </div>
+</template>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,49 @@
 <script setup>
 import Menu from '@/components/Menu.vue';
+import PizzaItem from '@/components/PizzaItem.vue';
+import { ref } from 'vue';
+
+const pizzas = ref([
+    {
+        id: 1,
+        nome: 'Calabresa',
+        preco: '19.90',
+        imagem: 'imagem',
+        promocao: false,
+        vegana: false,
+        doce: false
+    },
+    {
+        id: 2,
+        nome: 'Banana Nevada',
+        preco: '39.90',
+        imagem: 'imagem',
+        promocao: false,
+        vegana: false,
+        doce: true
+    }
+])
+
 
 </script>
 
 <template>
     <div class="body">
-        <Menu/>
-
+        <Menu />
+        <div class="container py-4">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+                <PizzaItem
+                    v-for="pizza in pizzas"
+                    :key="pizza.id"
+                    :nome="pizza.nome"
+                    :preco="pizza.preco"
+                    :imagem="pizza.imagem"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+
 </style>
